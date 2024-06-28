f332afd0737450ff9470ce5dde73649c400bb258
porota <ken887265@gmail.com>
2024-06-29 00:45:18 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import PostsList from './PostsList';
+import DetailsPage from './DetailsPage';
+import InquiryPage from './InquiryPage';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<PostsList />} />
+      <Route path="/post/:id" element={<DetailsPage  />} />
+      <Route path="/inquiry" element={<InquiryPage />} />
+    </Routes>
   );
-}
+};
 
 export default App;
